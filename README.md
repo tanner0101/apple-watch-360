b15ef4c6b75086e3463b1115c8fb692f3c8de9bd
Tanner Nelson <me@tanner.xyz>
2015-04-24 00:09:18 -0400

    sw33t

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# apple-watch-360
+#  Watch 360
+
+Copy and paste this JavaScript code into any of the Apple Watch pages with a 360 view to get a beautiful, automated viewing experience.
+
+- Tanner
---
+++ apple-watch-360.js
@@ -0,0 +1,57 @@
+//by tanner nelson
+
+var name_p = $('meta[property="og:image"]').content.split('/');
+var name = name_p[7];
+var main = document.getElementById("main");
+
+main.style.opacity = 0;
+var nav = document.getElementById("localnav");
+nav.style.opacity = 0;
+
+document.body.style.backgroundSize = "790px 790px";
+document.body.style.backgroundPosition = "center 110px";
+
+function urlforpos(pos) {
+    var leading = '000';
+    if (pos >= 100) {
+        leading = '0';
+    } else if (pos >= 10) {
+        leading = '00';
+    }
+    
+    var type = 'apple-watch-sport';
+    if (name.indexOf('steel') >= 0) { 
+        type = 'apple-watch'; 
+    } else if (name.indexOf('gold') >= 0) { 
+        type = 'apple-watch-edition'; 
+        if (name.indexOf('18-karat') < 0) {
+            name = "18-karat-" + name;
+        }
+    }
+
+    return "http://images.apple.com/media/us/watch/2015/a718f271_b19c_47d8_928d_d108fc5d702a/product-viewer/"+type+"/" + name + "/360/large_2x/jpg/" + name + "_" + leading + pos + ".jpg";
+}
+function preloadImage(url) {
+                var img = new Image();
+                img.src = url;
+}
+var pos = 0;
+
+function load() {
+    
+    for(var i = 0; i <= 360; i++) {
+        preloadImage(urlforpos(i));
+    }
+    spin();
+
+}
+function spin() {
+
+    document.body.style.backgroundImage = "url("+urlforpos(pos)+")";
+    setTimeout(function(){ spin() }, 80);
+    pos += 1;
+    if(pos>360) { pos = 0 };  
+
+}
+
+load();
